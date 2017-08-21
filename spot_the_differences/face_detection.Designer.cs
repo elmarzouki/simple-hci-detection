5843f70feff6d48f57daf1aedba1e2216a971f88
iSuperMostafa <mostafam.elmarzouki@gmail.com>
2017-08-21 14:48:36 +0200

    windows forms - start

---
+++ spot_the_differences/face_detection.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace spot_the_differences
+{
+    partial class face_detection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(face_detection));
+            this.face_detection_imageBox = new Emgu.CV.UI.ImageBox();
+            this.capture_radioButton = new System.Windows.Forms.RadioButton();
+            this.Next_button = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.face_detection_imageBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // face_detection_imageBox
+            // 
+            this.face_detection_imageBox.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.face_detection_imageBox.Location = new System.Drawing.Point(431, 179);
+            this.face_detection_imageBox.Name = "face_detection_imageBox";
+            this.face_detection_imageBox.Size = new System.Drawing.Size(517, 359);
+            this.face_detection_imageBox.TabIndex = 7;
+            this.face_detection_imageBox.TabStop = false;
+            // 
+            // capture_radioButton
+            // 
+            this.capture_radioButton.AutoSize = true;
+            this.capture_radioButton.BackColor = System.Drawing.Color.Transparent;
+            this.capture_radioButton.Font = new System.Drawing.Font("BN Deep Blue", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.capture_radioButton.ForeColor = System.Drawing.Color.FloralWhite;
+            this.capture_radioButton.Location = new System.Drawing.Point(394, 544);
+            this.capture_radioButton.Name = "capture_radioButton";
+            this.capture_radioButton.Size = new System.Drawing.Size(173, 38);
+            this.capture_radioButton.TabIndex = 8;
+            this.capture_radioButton.Text = "capture now";
+            this.capture_radioButton.UseVisualStyleBackColor = false;
+            this.capture_radioButton.CheckedChanged += new System.EventHandler(this.capture_radioButton_CheckedChanged);
+            // 
+            // Next_button
+            // 
+            this.Next_button.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.Next_button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Next_button.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Next_button.Font = new System.Drawing.Font("BN Deep Blue", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Next_button.Location = new System.Drawing.Point(895, 555);
+            this.Next_button.Name = "Next_button";
+            this.Next_button.Size = new System.Drawing.Size(84, 41);
+            this.Next_button.TabIndex = 6;
+            this.Next_button.Text = "Next";
+            this.Next_button.UseVisualStyleBackColor = false;
+            this.Next_button.Visible = false;
+            this.Next_button.Click += new System.EventHandler(this.Next_button_Click);
+            // 
+            // face_detection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1300, 600);
+            this.Controls.Add(this.face_detection_imageBox);
+            this.Controls.Add(this.capture_radioButton);
+            this.Controls.Add(this.Next_button);
+            this.DoubleBuffered = true;
+            this.KeyPreview = true;
+            this.Name = "face_detection";
+            this.Text = "face detection";
+            this.Load += new System.EventHandler(this.face_detection_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.face_detection_imageBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox face_detection_imageBox;
+        private System.Windows.Forms.RadioButton capture_radioButton;
+        private System.Windows.Forms.Button Next_button;
+    }
+}
---
+++ spot_the_differences/face_detection.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.Util;
+
+namespace spot_the_differences
+{
+    public partial class face_detection : Form
+    {
+        private Capture capture_cam = null;
+        bool capture_in_progress = false;
+        private HaarCascade frontal_face_detector = null;
+        MCvAvgComp[] detected_faces = null;
+        Image<Bgr, byte> original_img;
+        Image<Gray, byte> original_gray;
+        double scale = 1.1;
+        int min_neighbor = 2;
+        int win_size = 25;
+
+        public face_detection()
+        {
+            InitializeComponent();
+            frontal_face_detector = new HaarCascade(@"haarcascade_frontalface_default.xml");
+        }
+
+        private void face_detection_Load(object sender, EventArgs e)
+        {
+            FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+        }
+
+        private void Form_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                FormBorderStyle = System.Windows.Forms.FormBorderStyle.Sizable;
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+
+        private void capture_radioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            if (capture_radioButton.Checked)
+            {
+                capture_in_progress = true;
+                capture_cam = new Capture();
+            }
+            original_img = null;
+            original_gray = null;
+            Application.Idle += processing;
+        }
+
+        private void processing(object obj, EventArgs e)
+        {
+
+            if (capture_radioButton.Checked)
+            {
+                original_img = capture_cam.QueryFrame();
+            }
+            original_gray = original_img.Convert<Gray, byte>();
+            detected_faces = frontal_face_detector.Detect(original_gray, scale, min_neighbor, HAAR_DETECTION_TYPE.DO_CANNY_PRUNING, new Size(win_size, win_size), new Size(win_size, win_size));
+            if (detected_faces.Length > 0)
+            {
+                foreach (MCvAvgComp face in detected_faces)
+                {
+                    original_img.Draw(face.rect, new Bgr(Color.Red), 3);
+                    Next_button.Visible = true;
+                }
+            }
+            face_detection_imageBox.Image = original_img.Resize(face_detection_imageBox.Width, face_detection_imageBox.Height, INTER.CV_INTER_LINEAR);
+        }
+
+        private void Next_button_Click(object sender, EventArgs e)
+        {
+            this.Visible = false;
+            shape_detection shapeDetiction = new shape_detection();
+            shapeDetiction.Visible = true;
+        }
+
+        
+
+    }
+}
